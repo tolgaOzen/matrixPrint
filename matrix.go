89de6a4ce4ab1c8240341f59118672126a280bda
tolga ozen <tolga@hedef.in>
2018-06-07 13:52:52 +0300

    after every word go through the bottom line

--- matrix.go
+++ matrix.go
@@ -1,16 +1,23 @@
 package matrix
 
 import (
-"strings"
-"time"
-"fmt"
+	"strings"
+	"time"
+	"fmt"
 )
 
 func MPrint(val string) {
 	t := strings.Split(val, "")
+
 	for _, a := range t {
+
+
 		var c = time.Second * 1 / 20
+
 		time.Sleep(c)
+
 		fmt.Print(a)
+
 	}
+	fmt.Print("\n")
 }
